04b3a1f5a309f6d564a4eaf47cb41d2e8f5efa3f
Vijay Kumar <chunchuvijaykumar96@gmail.com>
2021-11-19 11:29:15 -0600

    added buttons

--- routes/Rabbit.js
+++ routes/Rabbit.js
@@ -3,7 +3,9 @@ const rabbit_controller = require('../controllers/rabbit');
 var router = express.Router();
 
 // router.get('/delete', rabbit_controller.rabbit_delete_Page);
-router.get('/detail', rabbit_controller.rabbit_view_one_Page);
+router.get('/', rabbit_controller.rabbit_view_all_Page);
+router.get('/update', rabbit_controller.rabbit_update_Page);
+
 
 
 module.exports = router; 
