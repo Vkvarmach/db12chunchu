0b617c0e97ad6d9d847cb4419f153a6297e9e5a3
Vijay Kumar <chunchuvijaykumar96@gmail.com>
2021-12-02 00:01:10 -0600

    updated

--- routes/Rabbit.js
+++ routes/Rabbit.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // router.get('/delete', rabbit_controller.rabbit_delete_Page);
 router.get('/', rabbit_controller.rabbit_view_all_Page);
- 
+
 const secured = (req, res, next) => {
 
     if (req.user){
@@ -14,6 +14,7 @@ const secured = (req, res, next) => {
     res.redirect("/login");
     }
 router.get('/update', secured, rabbit_controller.rabbit_update_Page);
+router.get('/create', secured, rabbit_controller.rabbit_create_Page);
 
 
 
